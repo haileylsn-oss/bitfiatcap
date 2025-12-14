615e2fff9c4bf0a48600a05b6c996355959bdbe5
haileylsn-oss <haileylsn@gmail.com>
2025-12-14 21:48:45 +0100

    pp

--- src/components/support.tsx
+++ src/components/support.tsx
@@ -1,36 +1,36 @@
+declare global {
+  interface Window {
+    smartsupp: any;
+    _smartsupp: any;
+  }
+}
+
 import { useEffect } from "react";
 import { FaEnvelope } from "react-icons/fa";
 
 const SupportBot = () => {
   useEffect(() => {
+    if (window.smartsupp) return;
+
+    window._smartsupp = window._smartsupp || {};
+    window._smartsupp.key = "8d05adc4a0b26322e115c3c5b11449d3ab26e675";
+
     const script = document.createElement("script");
-    script.type = "text/javascript";
     script.async = true;
-    script.innerHTML = `
-      var _smartsupp = _smartsupp || {};
-      _smartsupp.key = '8d05adc4a0b26322e115c3c5b11449d3ab26e675';
-      window.smartsupp||(function(d) {
-        var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-        s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-        c.type='text/javascript';c.charset='utf-8';c.async=true;
-        c.src='https://www.smartsuppchat.com/loader.js?';
-        s.parentNode.insertBefore(c,s);
-      })(document);
-    `;
+    script.src = "https://www.smartsuppchat.com/loader.js";
+
     document.body.appendChild(script);
   }, []);
 
   return (
-    <>
-      <div className="fixed bottom-[100px] left-5 z-50 flex flex-col items-center space-y-1">
-        <a href="mailto:danbraunstein57@gmail.com">
-          <button className="p-3 rounded-full shadow-lg bg-blue-800 hover:bg-blue-700 text-white">
-            <FaEnvelope />
-          </button>
-        </a>
-        <span className="text-sm text-black font-bold">Email Us</span>
-      </div>
-    </>
+    <div className="fixed bottom-[100px] left-5 z-50 flex flex-col items-center space-y-1">
+      <a href="mailto:danbraunstein57@gmail.com">
+        <button className="p-3 rounded-full shadow-lg bg-blue-800 hover:bg-blue-700 text-white">
+          <FaEnvelope />
+        </button>
+      </a>
+      <span className="text-sm text-black font-bold">Email Us</span>
+    </div>
   );
 };
 
